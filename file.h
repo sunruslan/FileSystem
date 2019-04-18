95c3f0d932d5184d1beed024acfbc7548ffc9e51
sunruslan <sunruslan331@gmail.com>
2019-04-18 23:31:36 +0300

    add file

---
+++ file.h
@@ -0,0 +1,15 @@
+//
+// Created by sunruslan on 4/18/19.
+//
+
+#ifndef FILESYSTEM_FILE_H
+#define FILESYSTEM_FILE_H
+
+#include "file_system.h"
+
+struct File{
+    int inode_number;
+    char* name[MAX_FILE_NAME_LENGTH];
+};
+
+#endif //FILESYSTEM_FILE_H
